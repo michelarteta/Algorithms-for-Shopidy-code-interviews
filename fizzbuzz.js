aa6e8268d059639c2ec733227d709418c7b63ea7
artetam <michel.arteta@dowjones.com>
2018-08-02 12:30:58 -0400

    add comments

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,18 +3,30 @@
 //and for the multiples of five print “Buzz”. 
 //For numbers which are multiples of both three and five print “FizzBuzz”."
 
-function fizzBuzz(number) {
+function fizzBuzz(n) {
+  
   let i;
-  for (i = 1; i <= number; i++) {
+  
+  for (i = 1; i <= n; i++) {
+
     if(i % (3 * 5) === 0 ) {
+      //For numbers which are multiples 
+      //of both three and five print “FizzBuzz”."
       console.log('Fizz Buzz');
+
     } else if (i % 5 === 0) {
+      //for the multiples of five 
+      //print “Buzz”. 
       console.log('Buzz');
+
     } else if (i % 3 === 0) {
+      //for multiples of three 
+      //print “Fizz”
       console.log('Fizz')
     } else {
       console.log(i)
     }
+
   }
 }
 
