c837c9952440a947056954e57432b1d21dcd40c2
artetam <michel.arteta@dowjones.com>
2018-08-13 09:34:01 -0400

    harmlessRansomNote

---
+++ harmlessRansomNote.js
@@ -0,0 +1,37 @@
+
+
+const arryA = 'this is a secret note for you from a secret admirer';
+const arryB = 'puerto rico is a place of great wonder and excitement it has many secret waterfall locatoins that i am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places i have ever visited';
+
+
+function harmlessRansomNote(noteText, magazineText) {
+
+  var noteArr = noteText.split(' ');
+  var magazineArr = magazineText.split(' ');
+
+  // console.log(noteArr, magazineArr);
+
+  var magazineObj = {};
+  
+  magazineArr.forEach(word => {
+    if (!magazineObj[word]) magazineObj[word] = 0;
+    magazineObj[word]++;
+  });
+
+  // console.log(magazineObj);
+  
+  var noteIsPossible = true;
+
+  noteArr.forEach(word => {
+    if (magazineObj[word]) {
+      magazineObj[word]--;
+      if (magazineObj[word] < 0) noteIsPossible = false;
+    }
+    else noteIsPossible = false; 
+  });
+  
+  return noteIsPossible;
+}
+
+ 
+harmlessRansomNote(arryA, arryB);
