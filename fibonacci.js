1164f3c103f2eaadc5aa7994880589ef22b374e7
artetam <michel.arteta@dowjones.com>
2018-08-13 09:52:21 -0400

    Add non optimized solutuion

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,17 @@
+function fibonacci(index) {
+	console.log(new Date())
+    if (index < 3) {
+    	return 1
+    } else {
+       console.log(new Date())
+       return fibonacci(index - 1) + fibonacci(index - 2);
+    }
+}
+ 
+fibonacci(5);
+
+
+// Optimized Algorithm
 function fibonacci(index, cache) {
   cache = cache || [];
   if (cache[index]) return cache[index];
