7bfe425a412161415f9662043bcfb843986888d3
Michel Arteta <michel.arteta@dowjones.com>
2019-10-19 10:09:15 -0400

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,4 @@
+// For Shopify Frontend Developer positions
 //"Write a program that prints the numbers from 1 to n. 
 //But for multiples of three print “Fizz” instead of the number 
 //and for the multiples of five print “Buzz”. 
